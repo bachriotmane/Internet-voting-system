9f57800076e4714ff95a0dd50caa116358dfbf10
ARAMALI Mohammed <mouhammedaramali@gmail.com>
2024-05-01 15:58:43 +0100

    complete server side room

--- Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/PrgrammeController.java
+++ Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/PrgrammeController.java
@@ -1,7 +1,35 @@
 package org.fsts.internet_voting_system_backend.controllers;
 
+import jakarta.servlet.http.HttpServlet;
+import lombok.AllArgsConstructor;
+import org.fsts.internet_voting_system_backend.entities.Programme;
+import org.fsts.internet_voting_system_backend.repositories.RoomRepository;
+import org.fsts.internet_voting_system_backend.services.RoomService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController("/programmes")
+@AllArgsConstructor
 public class PrgrammeController {
+    private final RoomService roomService;
+@GetMapping("/roomId")
+    public ResponseEntity<?> getProgrammesByRoom(@PathVariable String roomId)
+    {
+        Optional<List<Programme>> programmes = roomService.getProgrammesByRoom(roomId);
+
+        if(programmes.isPresent())
+        {
+            return new ResponseEntity<>(programmes.get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("there no programme in this room *_*",HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/RoomController.java
+++ Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/RoomController.java
@@ -3,6 +3,7 @@ package org.fsts.internet_voting_system_backend.controllers;
 
 import lombok.AllArgsConstructor;
 import org.fsts.internet_voting_system_backend.entities.Room;
+import org.fsts.internet_voting_system_backend.services.RoomService;
 import org.fsts.internet_voting_system_backend.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,22 +20,45 @@ import java.util.Optional;
 @AllArgsConstructor
 public class RoomController {
     public final UserService userService;
+    public final RoomService roomService;
 
     @GetMapping("/")
-    public List<Room> getAllRooms(){
-        return null;
+    public ResponseEntity<?>  getAllRooms(){
+        Optional<List<Room>>  rooms = roomService.getAllRoom();
+
+        if(rooms.isPresent())
+        {
+            return new  ResponseEntity(rooms.get(),HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("No Rooms Available ",HttpStatus.NOT_FOUND);
+        }
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/user/{userId}")
     public ResponseEntity<?> getCraetedRooms(@PathVariable String uderId)
     {
-       Optional<List<Room>> rooms = userService.getCreatedRooms(uderId);
+        Optional<List<Room>> rooms = userService.getCreatedRooms(uderId);
+        if(rooms.isPresent())
+        {
+            return new ResponseEntity<>(rooms.get(), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("there is no rooms with this user id ",HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+    @GetMapping("/search/{key}")
+    public ResponseEntity<?> getRoomsByKey(@PathVariable String key)
+    {
+        Optional<List<Room>> rooms = roomService.getRoomsByKeyword(key);
        if(rooms.isPresent())
        {
            return new ResponseEntity<>(rooms.get(), HttpStatus.OK);
        }
        else {
-           return new ResponseEntity<>("there is no rooms with this user id ",HttpStatus.NOT_FOUND);
+           return new ResponseEntity<>("there is no rooms with the title ",HttpStatus.NOT_FOUND);
        }
 
 
