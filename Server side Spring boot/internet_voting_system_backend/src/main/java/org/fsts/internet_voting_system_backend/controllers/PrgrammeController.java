a45e35fdf5dc3b0dec5cdeffc8ce27e9f210feaa
bachriotmane <bachriotmane@gmail.com>
2024-05-02 14:41:50 +0100

    solve conf

--- Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/PrgrammeController.java
+++ Server side Spring boot/internet_voting_system_backend/src/main/java/org/fsts/internet_voting_system_backend/controllers/PrgrammeController.java
@@ -10,26 +10,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController("/programmes")
+@RequestMapping("/programmes")
+@RestController
 @AllArgsConstructor
 public class PrgrammeController {
     private final RoomService roomService;
-@GetMapping("/{roomId}")
-    public ResponseEntity<?> getProgrammesByRoom(@PathVariable("roomId") String roomId)
-    {
-        Optional<List<Programme>> programmes = roomService.getProgrammesByRoom(roomId);
-
-        if(programmes.isPresent())
+        @GetMapping("/{roomId}")
+        public ResponseEntity<?> getProgrammesByRoom(@PathVariable("roomId") String roomId)
         {
-            return new ResponseEntity<>(programmes.get(), HttpStatus.OK);
+            Optional<List<Programme>> programmes = roomService.getProgrammesByRoom(roomId);
+
+            if(programmes.isPresent())
+            {
+                return new ResponseEntity<>(programmes.get(), HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>("there no programme in this room *_*",HttpStatus.NOT_FOUND);
+            }
         }
-        else{
-            return new ResponseEntity<>("there no programme in this room *_*",HttpStatus.NOT_FOUND);
-        }
-    }
 }
