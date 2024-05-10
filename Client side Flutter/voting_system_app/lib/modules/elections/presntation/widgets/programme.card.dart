9d4e8736e25a20926b014b608597afb1fca2d9ec
bachriotmane <bachriotmane@gmail.com>
2024-05-10 18:03:40 +0100

    fix bug

--- Client side Flutter/voting_system_app/lib/modules/elections/presntation/widgets/programme.card.dart
+++ Client side Flutter/voting_system_app/lib/modules/elections/presntation/widgets/programme.card.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:voting_system_app/modules/elections/domain/entities/programme.dart';
 
 class ProgrammeCard extends StatelessWidget {
